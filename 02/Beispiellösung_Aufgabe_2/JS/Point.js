808bdce177e95c736fca4b5785df672e07434b92
Jan-Patrick Bollow <jan_patrickbollow@hotmail.com>
2017-05-21 23:47:51 +0200

    21.05

---
+++ 02/Beispiellösung_Aufgabe_2/JS/Point.js
@@ -0,0 +1,9 @@
+/**  
+* @desc Class for a simple point with lat/long values
+* @author Steffen loos s_loos02@uni-muenster.de
+*
+*/
+function Point (lat, lon) {
+    this.lat = lat;
+    this.lon = lon;
+}
