52a7242ea36883482a5fcbc4e9596d4f4e4f2300
Jan-Patrick Bollow <jan_patrickbollow@hotmail.com>
2017-05-29 20:14:15 +0200

    Cleaner

--- 02/Beispiellösung_Aufgabe_2/JS/Point.js
+++
@@ -1,9 +0,0 @@
-/**  
-* @desc Class for a simple point with lat/long values
-* @author Steffen loos s_loos02@uni-muenster.de
-*
-*/
-function Point (lat, lon) {
-    this.lat = lat;
-    this.lon = lon;
-}
