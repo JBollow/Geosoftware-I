11d5d74c03ed624c4b2962b8276e2461580f41d4
Jan-Patrick Bollow <jan_patrickbollow@hotmail.com>
2017-06-15 05:01:13 +0200

    15.06 - #5 - v0.4

--- 05/js/position.js
+++ 05/js/position.js
@@ -10,7 +10,7 @@ function initPosition() {
 
         // Output text + moveto button
         function (position) {
-            document.getElementById('positionbox').innerHTML = '<p>Latitude: ' + Math.round(position.coords.latitude * 10000) / 10000 + '&nbsp;&#x2194;&nbsp;Longitude: ' + Math.round(position.coords.longitude * 10000) / 10000 + '&nbsp;&nbsp;<input type="button" class="button" value="Jump to my location" onclick="moveToLocation(' + position.coords.latitude + "," + position.coords.longitude + ')"/>';
+            document.getElementById('positionbox').innerHTML = '<p>Latitude: ' + Math.round(position.coords.latitude * 10000) / 10000 + '<br>Longitude: ' + Math.round(position.coords.longitude * 10000) / 10000 + '<br><input type="button" class="button" style="width: 100%;" value="Jump to my location" onclick="moveToLocation(' + position.coords.latitude + "," + position.coords.longitude + ')"/>';
 
             //JSNLog
             logger.info("Location found");
