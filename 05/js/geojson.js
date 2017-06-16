4ea98c81f1d30e92e5f528470944a12c29565118
Jan-Patrick Bollow <jan_patrickbollow@hotmail.com>
2017-06-16 03:05:34 +0200

    Added 2nd map, fixed nav, addes geojson loader

--- 05/js/geojson.js
+++ 05/js/geojson.js
@@ -8,6 +8,15 @@
 function geoJsonLink() {
     // getting the link from the textinput
     var link = $("#jsonLink").val();
+
     // pushing the linked geojson to the map
     map.data.loadGeoJson(link);
+
+    // setting a style for the GeoJson
+    map.data.setStyle({
+        fillColor: '#4CAF50',
+        fillOpacity: 0.5,
+        strokeColor: '#336633',
+        strokeWeight: 2,
+    })
 };
